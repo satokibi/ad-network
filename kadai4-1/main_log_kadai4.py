778eee967a0390931dbbda7c19228679f8156e06
yuya higuchi <td182007@cis.fukuoka-u.ac.jp>
2018-07-19 11:56:09 +0900

    fix kadai4

--- kadai4-1/main_log_kadai4.py
+++ kadai4-1/main_log_kadai4.py
@@ -24,6 +24,17 @@ if __name__=='__main__':
 	drop04packet = 0
 	sum_loss_time = 0
 
+	jitter = 0
+
+	drop_packet = 0
+	time_list = []
+	through_put_list = []
+	drop_num_list = []
+	receive_packet_num = []
+	loss_time_ave_list = []
+	loss_time_list = []
+	jitter_list = []
+
 	line = raw_input()
 	start_time = line.split()[1]
 
@@ -31,6 +42,14 @@ if __name__=='__main__':
 		ls = line.split()
 		end_time = ls[1]
 		log = PacketLog(ls[0], ls[1], ls[2], ls[3], ls[4], int(ls[5]), ls[6], ls[7], ls[8], ls[9], ls[10], ls[11])
+
+		time_list.append(log.time)
+		if node4.get_packet() != 0 and (float(end_time) - float(start_time)) != 0:
+			through_put_list.append(round((node4.get_packet() * 8) / (float(end_time) - float(start_time))/1000, 1))
+		else:
+			through_put_list.append(0)
+		drop_num_list.append(drop_packet)
+
 
 		if log.event == '+':
 			if log.link_src == '0' and log.link_dst == '2':
@@ -69,11 +88,19 @@ if __name__=='__main__':
 					if l.pck_id == log.pck_id:
 						loss_time = float(log.time) - float(l.time)
 						sum_loss_time += loss_time
+
+						jitter = jitter + ((abs(loss_time) - jitter) / 16)
+						# receive_packet_num.append(node4.receive + 1)
+						receive_packet_num.append(log.time)
+						loss_time_ave_list.append(sum_loss_time / (node4.receive + 1) * 1000)
+						loss_time_list.append(loss_time * 1000)
+						jitter_list.append(jitter * 1000)
 				node4.receive_p(log.pck_size)
 			elif log.link_src == '3' and log.link_dst == '5':
 				node5.receive_p(log.pck_size)
 
 		if log.event == 'd':
+			drop_packet += 1
 			if log.src_port == '0.0' and log.dst_port == '4.0':
 				drop04packet += 1
 
@@ -103,9 +130,13 @@ if __name__=='__main__':
 	print('loss / get (num) = ' + str(drop04packet) + ' / ' + str(queue02.en))
 	print('loss / get (%)   = ' + str(100 * round(float(drop04packet)/float(queue02.en),5)) + ' %')
 	print(' - - - - - - - - - - - - - - - - - ')
-	print('total loss time(sec) = ' + str(sum_loss_time))
+	print('total loss time(sec)  = ' + str(sum_loss_time))
 	print('node4 receive pck num = ' + str(node4.receive))
-	print('ave loss time(sec) = ' + str(sum_loss_time / node4.receive))
+	print('ave loss time(sec)  = ' + str(sum_loss_time / node4.receive))
+	print('ave loss time(msec) = ' + str((sum_loss_time / node4.receive)*1000))
+	print(' - - - - - - - - - - - - - - - - - ')
+	print('jitter(sec)  = ' + str(jitter))
+	print('jitter(msec) = ' + str(jitter * 1000))
 	print(' - - - - - - - - - - - - - - - - - ')
 	print('get_packet(bit)  = ' + str(node4.get_packet() * 8) + ' bit')
 	print('get_packet(byte) = ' + str(node4.get_packet()) + ' byte')
@@ -114,3 +145,30 @@ if __name__=='__main__':
 	print('through put = '+ str(round(through_put/1000, 1)) + 'kbps')
 	print('through put = '+ str(round(through_put/1000000, 3))  + 'Mbps')
 	print(' - - - - - - - - - - - - - - - - - ')
+
+	fig = plt.figure(figsize=(12, 8))
+	axL = fig.add_subplot(1,2,1)
+	axR = fig.add_subplot(1,2,2)
+
+	axL.plot(time_list, through_put_list, label='throughput (kbps) ')
+	axL2 = axL.twinx()
+	axL2.plot(time_list, drop_num_list, label='drop pck total num', color='red')
+	axL2.axis(ymin=0,ymax=drop_packet + 5)
+
+	axL.legend(bbox_to_anchor=(1, 0.1))
+	axL2.legend(bbox_to_anchor=(1, 0.2))
+	axL.set_xlabel('Time (sec)')
+	axL.set_ylabel('kbps')
+	axL2.set_ylabel('drop pck total num')
+
+	axR.plot(receive_packet_num, loss_time_ave_list, label='loss ave time')
+	axR.plot(receive_packet_num, loss_time_list, label='loss time')
+	axR.plot(receive_packet_num, jitter_list, label='jitter')
+	axR.set_xlabel('Time (sec)')
+	axR.set_ylabel('msec')
+	# axR.legend(bbox_to_anchor=(1, 0.2))
+	axR.legend(loc='best')
+
+	plt.subplots_adjust(wspace=0.6)
+	plt.show()
+
